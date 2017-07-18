b93719228dee4f3cc936e14526508f5cf84cfb88
Victor Gualberto <victorgualbertolive@gmail.com>
2017-07-18 16:25:24 -0300

    modified bconvertd func

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -1,21 +1,17 @@
 #Main() project
+from string import ascii_uppercase, digits
+
 def bconvertd(val,base):
     #initializing variables
     if base<2 or base>36:
         return 'ERROR: base must be betwen 2 and 36'
-    alphanum=''
-    letter='A'
-    for i in range(10):
-        alphanum += str(i)
-    for i in range(26):
-        alphanum += letter
-        letter=chr(ord(letter)+1)
+    alphanum=digits+ascii_uppercase
     
     dec=0
     exp=1
-    val=str(val)
-    i=len(val)-1
+    val=str(val).upper()
     
+    i=len(val)-1
     while i >= 0:
         dec+=int(alphanum.index(val[i]))*(exp)
         exp*=base
